8ca569087bff0a52e1c6b78fd10a9ef978b3c89d
Christian Viana <viana2015christian@gmail.com>
2023-08-31 15:29:36 -0300

    Iniciado login com google sem context

--- src/pages/LoginCandidato/index.jsx
+++ src/pages/LoginCandidato/index.jsx
@@ -1,13 +1,23 @@
 
 import IMGLogo from '../../assets/logo.png'
 import { Container, Content } from './styles'
-
+import { singInWithGooglePopup  } from '../../service/firebase'
+import { useNavigate } from 'react-router-dom'
 export function LoginCandidato() {
+
+  const navigate = useNavigate()
+  const logGoogleUser = async () => {
+    const reponse = await singInWithGooglePopup()
+
+    navigate("/cadidato/dashboard")
+  }
   return (
     <Container>
       <Content>
         <img src={IMGLogo}/>
-        Login com Google
+        <button onClick={logGoogleUser}>
+          Login com Google
+        </button>
       </Content>
     </Container>
   )
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,15 +1,23 @@
 import { initializeApp } from "firebase/app";
-
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE,
-  messagingSenderId: import.meta.env.VITE_MESSAGIG_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyCI2lVdoHBnTm2LVPJ8HvNoJCzjlntxLp8",
+  authDomain: "coderecrute.firebaseapp.com",
+  projectId: "coderecrute",
+  storageBucket: "coderecrute.appspot.com",
+  messagingSenderId: "1014678575511",
+  appId: "1:1014678575511:web:96e83bdea0f36bf3dc4cd2",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const singInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 export default app;
